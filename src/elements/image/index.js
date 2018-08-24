00f10ef013d853cf7efbbb32af379651bcbdb316
Shirshov Sergey <s.shirshov@targetix.net>
2018-08-24 03:11:00 +0300

    Неиспользуемые файлы

--- src/elements/image/index.js
+++
@@ -1,9 +0,0 @@
-import Component from './Component'
-import AddComponent from './AddComponent'
-
-export default {
-    type: 'Image',
-    resize: 'keep-aspect-ratio',
-    component: Component,
-    addComponent: Component
-}
